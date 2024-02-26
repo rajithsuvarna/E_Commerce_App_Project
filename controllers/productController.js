87c3f6c8c86a96916698b50a2673a72a977af5ad
Rajith Suvarna D <109891044+rajithsuvarna@users.noreply.github.com>
2024-02-26 22:02:50 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,10 @@
+const db = require("../db");
+
 module.exports.create = async function (req, res) {
   try {
+    await db.executQuery(`INSERT INTO product (title, price, description, availability, qty) 
+    VALUES ('${req.body.title}', ${req.body.price},'${req.body.description}',${req.body.availability},${req.body.qty});`);
+    res.sendStatus(200);
   } catch (err) {
     console.log(err);
     return;
@@ -8,13 +13,19 @@ module.exports.create = async function (req, res) {
 
 module.exports.getProduct = async function (req, res) {
   try {
+    let product = await db.executQuery(
+      `SELECT * FROM product where id='${req.params.id}'`
+    );
+    res.send(product);
   } catch (err) {
     console.log(err);
     return;
   }
 };
-module.exports.updateProduct = async function (req, res) {
+module.exports.getAllProduct = async function (req, res) {
   try {
+    let product = await db.executQuery(`SELECT * FROM product`);
+    res.send(product);
   } catch (err) {
     console.log(err);
     return;
@@ -22,6 +33,9 @@ module.exports.updateProduct = async function (req, res) {
 };
 module.exports.createCategory = async function (req, res) {
   try {
+    await db.executQuery(`INSERT INTO category (category_name,product_id) 
+    VALUES ('${req.body.name}','${req.body.product_id}')`);
+    res.sendStatus(200);
   } catch (err) {
     console.log(err);
     return;
@@ -29,6 +43,8 @@ module.exports.createCategory = async function (req, res) {
 };
 module.exports.getCategory = async function (req, res) {
   try {
+    let category = await db.executQuery(`SELECT * FROM category`);
+    res.send(category);
   } catch (err) {
     console.log(err);
     return;
