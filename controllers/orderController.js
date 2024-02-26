1a1351f21110b58b2f09ac4d39975ea6325ccb51
Rajith Suvarna D <109891044+rajithsuvarna@users.noreply.github.com>
2024-02-26 22:02:21 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,9 @@
+const db = require("../db");
 module.exports.placeOrder = async function (req, res) {
   try {
+    await db.executQuery(`INSERT INTO Myorder (user_id,total_price,payment_mode,address,status) 
+    VALUES ('${req.body.userid}','${req.body.price}', '${req.body.paymentmode}','${req.body.address}','${req.body.status}');`);
+    res.send(200);
   } catch (err) {
     console.log(err);
     return;
@@ -8,6 +12,8 @@ module.exports.placeOrder = async function (req, res) {
 
 module.exports.getOrder = async function (req, res) {
   try {
+    let product = await db.executQuery(`SELECT * FROM Myorder`);
+    res.send(product);
   } catch (err) {
     console.log(err);
     return;
