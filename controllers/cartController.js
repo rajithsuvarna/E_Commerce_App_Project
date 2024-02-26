481755f06b23237d106d77585fa3e6ea5550b5b8
Rajith Suvarna D <109891044+rajithsuvarna@users.noreply.github.com>
2024-02-26 22:01:56 +0530

    Update cartController.js

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,5 +1,9 @@
+const db = require("../db");
 module.exports.create = async function (req, res) {
   try {
+    await db.executQuery(`INSERT INTO cart (product_id,user_id,order_id,qty,total_price) 
+    VALUES ('${req.body.product_id}', ${req.body.user_id},'${req.body.order_id}',${req.body.qty},${req.body.total_price});`);
+    res.sendStatus(200);
   } catch (err) {
     console.log(err);
     return;
@@ -8,20 +12,23 @@ module.exports.create = async function (req, res) {
 
 module.exports.getCart = async function (req, res) {
   try {
+    let cart = await db.executQuery(
+      `SELECT * FROM cart where id=${req.params.id}`
+    );
+    res.send(cart);
   } catch (err) {
     console.log(err);
     return;
   }
 };
-module.exports.updateCart = async function (req, res) {
-  try {
-  } catch (err) {
-    console.log(err);
-    return;
-  }
-};
+
 module.exports.deleteCart = async function (req, res) {
   try {
+    await db.executQuery(
+      `DELETE FROM cart
+      WHERE id = ${req.params.id};`
+    );
+    res.sendStatus(200);
   } catch (err) {
     console.log(err);
     return;
