f2fd2b5a461f162a6d76d625512148adbc679354
Rajith Suvarna D <109891044+rajithsuvarna@users.noreply.github.com>
2024-02-26 22:04:49 +0530

    Update user.js

--- routes/Api/v1/user.js
+++ routes/Api/v1/user.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const user_Controller = require("../../../controllers/userController");
 
+
 router.post("/signup", user_Controller.create);
 router.get("/login", user_Controller.login);
 
 module.exports = router;
+
