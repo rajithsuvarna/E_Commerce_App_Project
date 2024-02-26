f04238621d29fcaae1f48d4712c21faefc623127
Rajith Suvarna D <109891044+rajithsuvarna@users.noreply.github.com>
2024-02-26 22:03:15 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,10 @@
 const db = require("../db");
+
 module.exports.create = async function (req, res) {
   try {
-    db.executQuery();
+    await db.executQuery(`INSERT INTO User (username,email, password) 
+    VALUES ('${req.body.username}','${req.body.email}', '${req.body.password}');`);
+    res.send(200);
   } catch (err) {
     console.log(err);
     return;
@@ -10,6 +13,15 @@ module.exports.create = async function (req, res) {
 
 module.exports.login = async function (req, res) {
   try {
+    let pass = await db.executQuery(
+      `SELECT password from User where email='${req.body.email}'`
+    );
+    if (pass === req.body.password) {
+      res.send("Login Successful");
+    } else {
+      res.send("Invalid credential");
+    }
+    return;
   } catch (err) {
     console.log(err);
     return;
