5c17bbb611bcba9094c6412985ded0dea8c60381
Rajith Suvarna D <109891044+rajithsuvarna@users.noreply.github.com>
2024-02-26 22:04:07 +0530

    Update cart.js

--- routes/Api/v1/cart.js
+++ routes/Api/v1/cart.js
@@ -4,7 +4,6 @@ const cart_Controller = require("../../../controllers/cartController");
 
 router.post("/create", cart_Controller.create);
 router.delete("/deletecart/:id", cart_Controller.deleteCart);
-router.post("/updatecart/:id", cart_Controller.updateCart);
 router.get("/getcart/:id", cart_Controller.getCart);
 
 module.exports = router;
